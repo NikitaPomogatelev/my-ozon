e3af66df25168726e0c93150646fd9bc07697253
Никита Помогателев <pomogatelev@brick.dev>
2021-12-17 19:39:06 +0500

    Делегирование событий

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,17 +1,63 @@
 const cart = () => {
     const cartBtn = document.querySelector('#cart')
     const cartModal = document.querySelector('.cart')
-    const cartBtnClose = document.querySelector('.cart-close')
 
+    const disableScroll = () => {
+
+        // Вычисление ширины скрола 
+        const widthScroll = window.innerWidth - document.body.offsetWidth;
+        // console.log(widthScroll);
+
+        document.disableScroll = true;
+
+        // Сколь px от верха прокручено
+        document.body.dbScrollY = window.scrollY;
+        document.body.style.cssText = `
+            position: fixed;
+            top: ${-window.scrollY}px;
+            left: 0;
+            width: 100%;
+            height: 100vh;
+            overflow: hidden;
+            padding-right: ${widthScroll}px;
+        `;
+    };
+    // Разблокировка скролла
+    const enableScroll = () => {
+        document.body.style.cssText = '';
+        window.scroll({
+            top: document.body.dbScrollY,
+        })
+    };
+
+    const escapeHandler = ({
+        code,
+        keyCode
+    }) => {
+        if (code === 'Escape' || keyCode === 27) {
+            closeCart();
+        }
+    }
+
+    
     const openCart = () => {
+        disableScroll()
         cartModal.classList.add('active')
+        document.addEventListener('keydown', escapeHandler);
     }
     const closeCart = () => {
+        enableScroll()
         cartModal.classList.remove('active')
+        document.removeEventListener('keydown', escapeHandler);
     }
+    
+    cartModal.addEventListener('click', ({ target }) => {
+        if (target.classList.contains('cart-close') || target === cartModal) {
+            closeCart();
+        }
+    });
 
     cartBtn.addEventListener('click', openCart)
-    cartBtnClose.addEventListener('click', closeCart)
 }
 
 export default cart
